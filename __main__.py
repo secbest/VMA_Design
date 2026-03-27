96cf74202a3c614c0bfedcacb250d9a19858ad28
Leon-Loh <lohzixiang24@gmail.com>
2026-03-27 14:00:14 +0800

    changes

--- __main__.py
+++ __main__.py
@@ -5,7 +5,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('includes/global/navbar.html')
+    return render_template('website/index.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('website/portfolio.html')
+
+@app.route('/studio')
+def studio():
+    return render_template('website/studio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('website/contact.html')
+
+@app.route('/e-quote')
+def e_quote():
+    return render_template('e-quote/e-quote.html')
 
 if __name__ == '__main__':
     app.run()
