2cb5833888411624c62942da01344b9277fe6a2c
Leon-Loh <lohzixiang24@gmail.com>
2026-03-27 13:41:50 +0800

    changes

--- __main__.py
+++ __main__.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('includes/global/navbar.html')
+
+if __name__ == '__main__':
+    app.run()
